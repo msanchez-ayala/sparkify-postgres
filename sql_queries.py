f109658a256a693da9af3c395f117f042e37474a
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-03-28 15:31:23 -0400

    finish all queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,20 +1,31 @@
-# DROP TABLES
+### DROP TABLES ###
 
-songplay_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
-# CREATE TABLES
+### CREATE TABLES ###
 
-songplay_table_create = ("""
-""")
+songplay_table_create = """
+    CREATE TABLE IF NOT EXISTS
+      songplays (
+        timestamp timestamp,
+        user_id int,
+        level text,
+        song_id varchar,
+        artist_id varchar,
+        session_id int,
+        location varchar,
+        user_agent varchar
+      )
+"""
 
 user_table_create = """
     CREATE TABLE IF NOT EXISTS
       users (
-        user_id int UNIQUE PRIMARY KEY,
+        user_id int,
         first_name text,
         last_name text,
         gender text,
@@ -25,7 +36,7 @@ user_table_create = """
 song_table_create = """
     CREATE TABLE IF NOT EXISTS
       songs (
-        song_id varchar UNIQUE PRIMARY KEY,
+        song_id varchar,
         title varchar,
         artist_id varchar,
         year int,
@@ -36,7 +47,7 @@ song_table_create = """
 artist_table_create = """
     CREATE TABLE IF NOT EXISTS
       artists (
-        artist_id varchar UNIQUE PRIMARY KEY,
+        artist_id varchar,
         name varchar,
         location varchar,
         latitude decimal,
@@ -47,7 +58,7 @@ artist_table_create = """
 time_table_create = """
     CREATE TABLE IF NOT EXISTS
       time (
-        timestamp timestamp UNIQUE PRIMARY KEY,
+        timestamp timestamp,
         hour int,
         day int,
         week_of_year int,
@@ -57,10 +68,24 @@ time_table_create = """
       )
 """
 
-# INSERT RECORDS
+### INSERT RECORDS ###
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = """
+    INSERT INTO
+      songplays (
+        timestamp,
+        user_id,
+        level,
+        song_id,
+        artist_id,
+        session_id,
+        location,
+        user_agent
+      )
+    VALUES
+      (%s, %s, %s, %s,
+       %s, %s, %s, %s)
+"""
 
 user_table_insert = """
     INSERT INTO
@@ -118,12 +143,36 @@ time_table_insert = """
        %s, %s, %s)
 """
 
-# FIND SONGS
+### FIND SONGS ###
 
-song_select = ("""
-""")
+song_select = """
+    SELECT
+      s.song_id,
+      s.artist_id
+    FROM
+      songs s
+    JOIN
+      artists a
+      USING (artist_id)
+    WHERE
+      s.title = %s AND
+      a.name = %s AND
+      s.duration = %s;
+"""
 
 # QUERY LISTS
 
-create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create] #[songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop] #[songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create
+]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop
+]
